3d0fe5ae7a8810f1f9ec876f26308ccd1ecad321
rmalmutairi <rmalmutairi@umass.edu>
2025-04-11 22:11:46 -0400

    HW2: encapsulate transactions list and return unmodifiable view

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -1,15 +1,16 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ExpenseTrackerModel {
 
-  public List<Transaction> transactions;
+  // make it private + final so no other class can touch or reassign it
+  private final List<Transaction> transactions = new ArrayList<>();
 
-  public ExpenseTrackerModel() {
-    transactions = new ArrayList<>(); 
-  }
+  // constructor no longer needs to re‑assign the field
+  public ExpenseTrackerModel() {}
 
   public void addTransaction(Transaction t) {
     transactions.add(t);
@@ -20,7 +21,7 @@ public class ExpenseTrackerModel {
   }
 
   public List<Transaction> getTransactions() {
-    return transactions;
+    return Collections.unmodifiableList(transactions);
   }
 
 }
